5f5f99cae69e75d279aec1193b366c6c1a2e747d
Geoffrey Noel <geoff.noel@protonmail.com>
2021-09-15 12:56:08 -0400

    Add configuration per host

--- xmobar.hs
+++ xmobar.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
 import Xmobar
 
 import Data.List (intercalate)
+import Network.HostName
 
 main :: IO ()
-main = xmobar config
+main = xmobar =<< configByHost
 
+-- The default config
 config :: Config
 config = defaultConfig
     { font = "xft:Fira Code:size=14:style=bold:antialias=true:autohint=false:rgba=rbg"
     , additionalFonts =
-        [ "xft:Mononoki Nerd Font:size=10:antialias=true:hinting=true"
-        , "xft:FontAwesome:size=10"
+        [ "xft:Mononoki Nerd Font:size=18:antialias=true:hinting=true"
+        , "xft:FontAwesome:size=18"
         ]
     , bgColor = "#292c3e"
     , fgColor = "#ebebeb"
@@ -24,7 +28,7 @@ config = defaultConfig
     , allDesktops = True
     , overrideRedirect = True
     , persistent = False
-    , iconRoot = "/home/gnoel/.config/xmobar/icons"
+    , iconRoot = "/home/gnoel/.config/xmobar/icons/default"
     , sepChar = "%"
     , alignSep = "}{"
     , template = templateLeft ++ " }{ " ++ templateRight
@@ -35,7 +39,7 @@ config = defaultConfig
             [ "-t", "<qualityipat> <fc=#98be65><ssid></fc>"
             , "--"
             , "--quality-icon-pattern", "<icon=network/wireless/%%.xpm/>"
-            ] 10
+            ] 5
 
         , Run $ DynNetwork
             [ "--template" , "<txipat> <rxipat>"
@@ -48,7 +52,7 @@ config = defaultConfig
             , "--"
             , "--rx-icon-pattern", "<icon=network/rx/%%.xpm/>"
             , "--tx-icon-pattern", "<icon=network/tx/%%.xpm/>"
-            ] 10
+            ] 5
 
         , Run $ Alsa "default" "Master"
             [ "--template", "<fn=1>\xfa7e</fn> <volume>% <status>"
@@ -73,8 +77,6 @@ config = defaultConfig
             , "-i",                  "<fc=#707880>IDLE</fc>"
             ] 10
 
-        -- , Run $ PipeReader "Vol:/tmp/xmobar.vol" "pr:volume"
-
         , Run $ StdinReader
         ]
     }
@@ -84,10 +86,30 @@ templateLeft = "<icon=haskell_20.xpm/> %StdinReader%"
 
 templateRight :: String
 templateRight = intercalate " <icon=separator.xpm/> "
-    --[ "%pr:volume%"
-    --, "%alsa:hw:sofhdadsp:Speaker%"
     [ "%alsa:default:Master%"
     , "%wlan0wi% %dynnetwork%"
     , "%battery%"
     , "%date%"
     ]
+
+monetConfig :: Config
+monetConfig = config
+    { iconRoot = "/home/gnoel/.config/xmobar/icons/monet"
+    }
+
+bachConfig :: Config
+bachConfig = config
+    { font =
+        "xft:Fira Code:size=8:style=bold:antialias=true:autohint=false:rgba=rbg"
+    , additionalFonts =
+        [ "xft:Mononoki Nerd Font:size=10:antialias=true:hinting=true"
+        , "xft:FontAwesome:size=10"
+        ]
+    , iconRoot = "/home/gnoel/.config/xmobar/icons/bach"
+    }
+
+configByHost :: IO Config
+configByHost = getHostName >>= \case
+    "monet" -> return monetConfig
+    "bach"  -> return bachConfig
+    _       -> return config
